3dc1ea3cf11b00f97d2645d43eb2e318e0f8fa3b
Maria <mariacapurro9@gmail.com>
2017-09-04 15:32:05 -0300

    se armo footer.php

---
+++ footer.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/styles.css" type="text/css">
+  <link rel="stylesheet" href="css/font-awesome/css/font-awesome.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,600" rel="stylesheet">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  <title>TeamApp!</title>
+</head>
+
+<body>
+
+  <footer>
+  <nav class="row">
+    <div class="col-xs-12 col-md-4">
+      <img class="logo" src="images/logo-home.png" alt="logo">
+    </div>
+    <div class="col-xs-6 col-sm-4 col-md-2">
+        <h3>Compañia</h3>
+      <ul>
+        <li><a href="#">Acerca de</a></li>
+        <li><a href="#">Prensa</a></li>
+        <li><a href="#">Noticias</a></li>
+      </ul>
+    </div>
+    <div class="col-xs-6 col-sm-4 col-md-2">
+        <h3>Comunidades</h3>
+      <ul>
+        <li><a href="#">Para deportistas</a></li>
+        <li><a href="#">Marcas</a></li>
+      </ul>
+    </div>
+    <div class="col-xs-6 col-sm-4 col-md-2">
+        <h3>Enlaces Utiles</h3>
+      <ul>
+        <li><a href="#">Ayuda</a></li>
+        <li><a href="#">Regalo</a></li>
+      </ul>
+    </div><br>
+    <div class="col-xs-12 col-md-2">
+      <ul style="float:right" class="listaFooter">
+        <li style="float:left"><img src="images/twitter.png" width="40px"><a href="http://twitter.com/"></a></li>
+        <li style="float:left"> <img src="images/face.png" width="40px"><a href="http://facebook.com/"></a></li>
+        <li style="float:left"> <img src="images/insta.png" width="40px"><a href="http://instagram.com/"></a></li>
+      </ul>
+    </div>
+  </nav><br>
+  <div class="col-xs-8 col-md-8">
+    <ul class="listaFooter">
+      <li style="float:left">
+         <a href="#">Legal</a>
+      </li>
+      <li style="float:left"> <a href="#">Privacidad</a></li>
+      <li style="float:left"> <a href="#">Cookies</a></li>
+      <li style="float:left"> <a href="#">Sobre anuncios</a></li>
+    </ul>
+  </div>
+    <div class="col-xs-4 col-md-4">
+      <small class="copyright" style="float:right">&copy; 2017 TEAMUP</small>
+      </div>
+
+
+
+  </footer>
+</body>
+
+</html>
