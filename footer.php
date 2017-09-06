890ee1341ac9f535e5a5e97ee7ce07c4964d4713
Ana Pfefferkorn <anapfefferkorn@gmail.com>
2017-09-06 01:32:02 -0300

    modifiqué footer, tengo version anterior guardada cualquier cosa

--- footer.php
+++ footer.php
@@ -1,56 +1,33 @@
-  <footer>
-  <nav class="row">
-    <div class="col-xs-12 col-md-4">
-      <img class="logo" src="images/logo-home.png" alt="logo">
+  <footer  class="container">
+    <div class="social-icons">
+        <a href="http://twitter.com/"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        <a href="http://facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        <a href="http://instagram.com/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
     </div>
-    <div class="col-xs-6 col-sm-4 col-md-2">
+    <div class="listaFooter">
         <h3>Compañia</h3>
-      <ul>
-        <li><a href="#">Acerca de</a></li>
-        <li><a href="#">Prensa</a></li>
-        <li><a href="#">Noticias</a></li>
-      </ul>
+        <ul>
+          <li><a href="#">Acerca de</a></li>
+          <li><a href="#">Prensa</a></li>
+          <li><a href="#">Noticias</a></li>
+        </ul>
     </div>
-    <div class="col-xs-6 col-sm-4 col-md-2">
+    <div class="listaFooter">
         <h3>Comunidades</h3>
-      <ul>
-        <li><a href="#">Para deportistas</a></li>
-        <li><a href="#">Marcas</a></li>
+        <ul>
+          <li><a href="#">Para deportistas</a></li>
+          <li><a href="#">Marcas</a></li>
+        </ul>
+    </div>
+    <div>
+      <ul class="listaFooter">
+        <li><a href="#">Legales</a></li>
+        <li><a href="#">Privacidad</a></li>
+        <li><a href="#">Cookies</a></li>
+        <li><a href="#">Sobre anuncios</a></li>
       </ul>
     </div>
-    <div class="col-xs-6 col-sm-4 col-md-2">
-        <h3>Enlaces Utiles</h3>
-      <ul>
-        <li><a href="#">Ayuda</a></li>
-        <li><a href="#">Regalo</a></li>
-      </ul>
-    </div><br>
-    <div class="col-xs-12 col-md-2">
-      <ul style="float:right" class="listaFooter">
-        <li style="float:left"><img src="images/twitter.png" width="40px"><a href="http://twitter.com/"></a></li>
-        <li style="float:left"> <img src="images/face.png" width="40px"><a href="http://facebook.com/"></a></li>
-        <li style="float:left"> <img src="images/insta.png" width="40px"><a href="http://instagram.com/"></a></li>
-      </ul>
+    <div>
+      <h6 class="copyright">Copyright © 2017, Created by LOS <strong>PICANTES</strong></h6>
     </div>
-  </nav><br>
-  <div class="col-xs-8 col-md-8">
-    <ul class="listaFooter">
-      <li style="float:left">
-         <a href="#">Legal</a>
-      </li>
-      <li style="float:left"> <a href="#">Privacidad</a></li>
-      <li style="float:left"> <a href="#">Cookies</a></li>
-      <li style="float:left"> <a href="#">Sobre anuncios</a></li>
-    </ul>
-  </div>
-    <div class="col-xs-4 col-md-4">
-      <small class="copyright" style="float:right">&copy; 2017 TEAMUP</small>
-      </div>
   </footer>
-
-  <div class="social-icons">
-    <i class="fa fa-facebook" aria-hidden="true"></i>
-    <i class="fa fa-instagram" aria-hidden="true"></i>
-    <i class="fa fa-twitter" aria-hidden="true"></i>
-  </div>
-</footer>
