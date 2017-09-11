fa10f26174d1c1cb27c898b804b5a0c498d411d0
dieduro <diegomduro@gmail.com>
2017-09-10 21:29:45 -0300

    footer Responsive Ok

--- footer2.php
+++ footer2.php
@@ -1,47 +1,49 @@
 
     <footer class="footer_outer">
-      <div class="footer_logo">
-        <img class="logo-foot" src="images/logo-home.png" alt="logo">
-      </div>
-      <div class="footer_cia">
-          <ul>
-            <li><h3>Compañia</h3></li>
-            <li><a href="#">Acerca de</a></li>
-            <li><a href="#">Prensa</a></li>
-            <li><a href="#">Noticias</a></li>
-          </ul>
-        </div>
-      <div class="footer_comunidades">
-          <ul>
-            <li><h3>Comunidades</h3></li>
-            <li><a href="#">Para deportistas</a></li>
-            <li><a href="#">Marcas</a></li>
-          </ul>
-      </div>
-      <div class="footer_enlaces">
-          <ul>
-            <li><h3>Enlaces Utiles</h3></li>
-            <li><a href="#">Ayuda</a></li>
-            <li><a href="#">Regalo</a></li>
-          </ul>
-      </div>
-      <div class="footer_redes">
+      <div class="redes">
         <ul>
           <li><a href="http://twitter.com/"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
           <li> <a href="http://facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
           <li><a href="http://instagram.com/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
         </ul>
       </div>
-      <div class="footer_legales">
+        <div class="footer_inner">
+          <div class="logo">
+            <img src="images/logo-home.png" alt="logo">
+          </div>
+          <div class="cia">
+              <ul>
+                <li><h3>Compañia</h3></li>
+                <li class="foot_links"><a href="#">Acerca de</a></li>
+                <li class="foot_links"><a href="#">Prensa</a></li>
+                <li class="foot_links"><a href="#">Noticias</a></li>
+              </ul>
+            </div>
+          <div class="comunidades">
+              <ul>
+                <li><h3>Comunidades</h3></li>
+                <li class="foot_links"><a href="#">Para deportistas</a></li>
+                <li class="foot_links"><a href="#">Marcas</a></li>
+              </ul>
+          </div>
+          <div class="enlaces">
+              <ul>
+                <li><h3>Enlaces Utiles</h3></li>
+                <li class="foot_links"><a href="#">Ayuda</a></li>
+                <li class="foot_links"><a href="#">Regalo</a></li>
+              </ul>
+          </div>
+        </div>
+      <div class="legales">
         <ul>
           <li> <a href="#">Legal</a></li>
           <li> <a href="#">Privacidad</a></li>
           <li> <a href="#">Cookies</a></li>
           <li> <a href="#">Sobre anuncios</a></li>
         </ul>
-        <div class="footer_copyright">
-          <small class="copyright">&copy;2017  TEAMUP</small>
-        </div>
+      </div>
+      <div class="copyright">
+        <p>&copy;2017  TEAMUP</p>
       </div>
   </footer>
 
@@ -51,10 +53,8 @@
       <!-- Include all compiled plugins (below), or include individual files as needed -->
       <script src="bootstrap/js/bootstrap.min.js"></script>
 
-      <script id="__bs_script__">
-        //<![CDATA[
-        document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.13'><\/script>".replace("HOST", location.hostname));
-        //]]>
-      </script>
+      <script id="__bs_script__">//<![CDATA[
+      document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.13'><\/script>".replace("HOST", location.hostname));
+  //]]></script>
 </body>
 </html>
